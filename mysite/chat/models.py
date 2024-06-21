daa185213a9ed7576aa4d2eea7d8c655235bfb5f
Usman <usmanmamatkulov4.com>
2024-06-22 00:09:02 +0600

    I add models.py

--- mysite/chat/models.py
+++ mysite/chat/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.PositiveSmallIntegerField(default=0, null=True, blank=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField(max_length=40, unique=True)
+
+
+class Chat(models):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to="img/", null=True, blank=True)
+    video = models.FileField(upload_to="video/", null=True, blank=True)
+    date = models.DateTimeField(default=datetime.today)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
