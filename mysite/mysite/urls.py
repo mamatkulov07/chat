5c81b728f46841d35a8ab4e4131a42c8efc59cf2
unknown <masterkak11110@gmail.com>
2024-06-22 18:18:43 +0600

    addfvgsddsagaasgerhe

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -21,7 +21,6 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView
 )
-from rest_framework import routers
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -45,5 +44,4 @@ urlpatterns = [
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('auth/', include('social_django.urls', namespace='social')),
 ]
