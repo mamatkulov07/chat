0adf7e7a8ae7e43f85e76d397393cbcbc7550227
Usman <usmanmamatkulov4.com>
2024-06-22 18:41:03 +0600

    azambek added docker

--- mysite/chat/serializers.py
+++ mysite/chat/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from .models import *
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
 
+User = get_user_model()
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
@@ -19,3 +22,26 @@ class MessageSerializer(serializers.ModelSerializer):
         model = Message
         fields = '__all__'
 
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'first_name', 'last_name')
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User.objects.create_user(**validated_data, password=password)
+        return user
+
+    def to_representation(self, instance):
+        refresh = RefreshToken.for_user(instance)
+
+        return {
+            "first_name": instance.first_name,
+            "last_name": instance.last_name,
+            "username": instance.username,
+            "email": instance.email,
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
